57846e42f342cd88699b40007a5a7037d87b12c4
Shivam Vishwakarma <recxcoder@gmail.com>
2026-05-28 22:21:59 +0530

    Day-1

---
+++ Day-1/Q1.java
@@ -0,0 +1,24 @@
+// Write a program to Calculate sum of first N natural number
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            sum += (i+1);
+        }
+
+        System.out.println("Sum of first " + n + " natural number = " + sum);
+
+        sc.close();
+    }
+    
+}
