2630d1e96a02c90c58a97ff835043e241613f52a
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-02 00:08:58 +0530

    Complete Q- 17, 18, 19

---
+++ Day5/Q20.java
@@ -0,0 +1,19 @@
+//Write a program to Find largest prime factor.
+
+package Day5;
+
+import java.util.Scanner;
+
+public class Q20 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+
+        sc.close();
+    }
+}
