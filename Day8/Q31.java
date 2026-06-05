c5d9dffc19ad890dab9deecc2956c495d2c76ee2
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-05 17:39:20 +0530

    Upload Day8

---
+++ Day8/Q31.java
@@ -0,0 +1,22 @@
+package Day8;
+
+/*
+Write a program to Print character triangle.
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+
+public class Q31 {
+    public static void main(String[] args) {
+        
+        for (char i = 'A'; i <= 'E' ; i++) {
+            for (char j = 'A'; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
