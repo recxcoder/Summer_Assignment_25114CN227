f6be81c2ecd5494cc32a54079027e8d7af439708
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-17 02:59:41 +0530

    Complete Q64

--- Day16/Q64.java
+++ Day16/Q64.java
@@ -3,5 +3,21 @@ package Day16;
 //Write a program to Remove duplicates from array.
 
 public class Q64 {
-    
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 20, 30, 30, 40, 40, 40, 50 };
+        int rd = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[rd]) {
+                rd++;
+                arr[rd] = arr[i];
+            }
+        }
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i <= rd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
