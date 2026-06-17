4b9823896f24f73a6b86885de56159cc44418a62
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-17 23:34:43 +0530

    Complete Q68

--- Day17/Q68.java
+++ Day17/Q68.java
@@ -1,7 +1,30 @@
 package Day17;
 
 //Write a program to Find common elements. 
+// This is similar question to Intersection of array
 
+import java.util.HashSet;
 public class Q68 {
-    
+    public static void main(String[] args) {
+        
+        String[] arr1 = {"Apple", "Mango", "Orange", "Banana"};
+        String[] arr2 = {"Mango", "Banana", "Pineapple", "Guava"};
+
+        HashSet<String> newSet = new HashSet<>();
+
+        for (String i : arr1) {
+            newSet.add(i);
+        }
+
+        System.out.print("Common elemets are: ");
+
+        for (int i = 0; i < arr2.length; i++) {
+            if (newSet.contains(arr2[i])) {
+                System.out.print(arr2[i] + " ");
+
+                // Remove the element to handle potential duplicates in arr2 gracefully
+                newSet.remove(arr2[i]);
+            }
+        }
+    }
 }
