3a305fe776ced736282f37e9cd2021f493c4cdf4
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-17 23:10:54 +0530

    Complete Q66

--- Day17/Q66.java
+++ Day17/Q66.java
@@ -2,6 +2,26 @@ package Day17;
 
 // Write a program to Union of arrays.
 
+import java.util.HashSet;
 public class Q66 {
-    
+    public static void main(String[] args) {
+        
+        int[] arr1 = {1, 2, 4, 5, 6};
+        int[] arr2 = { 2, 3, 5, 7};
+
+        HashSet<Integer> unionSet = new HashSet<>();
+
+        for (int i : arr1) {
+            unionSet.add(i);
+        }
+
+        for (int i : arr2) {
+            unionSet.add(i);
+        }
+
+        System.out.println("Union using HashSet: " + unionSet);
+    }
 }
+
+
+// HashSet refuses to store duplicate values. So HashSet is used in this problem.
