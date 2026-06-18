e79afe9b0f4182e6afc90758d2d1d7609592fc3c
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-18 15:52:51 +0530

    Complete Q72

--- Day18/Q72.java
+++ Day18/Q72.java
@@ -2,6 +2,26 @@ package Day18;
 
 //Write a program to Sort array in descending order.
 
+import java.util.Arrays;
 public class Q72 {
-    
+    public static void main(String[] args) {
+
+        int[] arr = { 3, 5, 2, 6, 4, 1, 9, 8, 7 };
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+        System.out.println("Sort array in desecnding order: " + Arrays.toString(arr));
+
+    }
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
