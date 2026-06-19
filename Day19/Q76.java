709fef68ec99af59fb00c1fc2d01e6eaddd2b4a6
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-19 13:31:23 +0530

    Complete Q76

--- Day19/Q76.java
+++ Day19/Q76.java
@@ -3,5 +3,23 @@ package Day19;
 //Write a program to Find diagonal sum. 
 
 public class Q76 {
-    
+    public static void main(String[] args) {
+
+        int[][] matrix1 = { { 1, 2, 3 },
+                            { 4, 5, 6 },
+                            { 7, 8, 9 } };
+
+        int rows = matrix1.length;
+        int cols = matrix1[0].length;
+        int diagonalSum = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == j) {
+                    diagonalSum += matrix1[i][j];
+                }
+            }
+        }
+        System.out.println("Diagonal sum of matrix is: " + diagonalSum);
+    }
 }
