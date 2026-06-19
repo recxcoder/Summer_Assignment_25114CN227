ba7885177e1daded30282a5f6343fc42bb0321ff
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-19 13:25:28 +0530

    Complete Q75

--- Day19/Q75.java
+++ Day19/Q75.java
@@ -3,5 +3,30 @@ package Day19;
 //Write a program to Transpose matrix.
 
 public class Q75 {
-    
+    public static void main(String[] args) {
+        
+        int[][] matrix = {{1,2,3},
+                          {4,5,6},
+                          {7,8,9}};
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+        System.out.println("Transpose of matrix is:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
