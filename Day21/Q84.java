3c396497adf6d22d4c6786b44c39fc03fa19421a
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-21 16:17:42 +0530

    Complete Q84

--- Day21/Q84.java
+++ Day21/Q84.java
@@ -3,5 +3,13 @@ package Day21;
 // Write a program to Convert lowercase to uppercase.
 
 public class Q84 {
-    
+    public static void main(String[] args) {
+        
+        String text = "I am doing my assignment.";
+
+        String upperText = text.toUpperCase();
+
+        System.out.println("\"" + text + "\" to upper case is: " + upperText);
+
+    }
 }
