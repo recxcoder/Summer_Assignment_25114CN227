18ba5ca6a0f8f764cae50add2346cf6b0b1bc9da
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-21 15:48:32 +0530

    Complete Q82

--- Day21/Q82.java
+++ Day21/Q82.java
@@ -3,5 +3,26 @@ package Day21;
 // Write a program to Reverse a string.
 
 public class Q82 {
-    
+    public static void main(String[] args) {
+        
+        String text = "Hello everyone!";
+
+        char[] chars = text.toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+
+        
+        while(left<right){
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            
+            left++;
+            right--;
+        }
+        
+        String reversed = new String(chars);
+        System.out.print("Reverse of " + text + " is: " + reversed);
+    }
 }
