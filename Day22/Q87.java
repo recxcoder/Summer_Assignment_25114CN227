8ab3e3380fb4d5212c69299a7b5422374f34290f
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-22 22:36:28 +0530

    Complete Q87

--- Day22/Q87.java
+++ Day22/Q87.java
@@ -2,6 +2,32 @@ package Day22;
 
 //Write a program to Character frequency
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 public class Q87 {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Write text: ");
+        String text = sc.nextLine();
+
+        String lowerText = text.toLowerCase();
+
+        Map<Character, Integer> freMap = new HashMap<>();
+
+        for (char c : lowerText.toCharArray()) {
+            if (c>= 'a' && c <='z') {
+                freMap.put(c, freMap.getOrDefault(c, 0) + 1);
+            }
+        }
+        
+        System.out.println("Character frequency: ");
+        for (Map.Entry<Character, Integer> entry : freMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        sc.close();
+    }
 }
