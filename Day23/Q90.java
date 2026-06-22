fdf8e40222bf1db08d8592b8d73b4bd9dc579e24
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-23 05:23:50 +0530

    Complete Q90

--- Day23/Q90.java
+++ Day23/Q90.java
@@ -2,6 +2,40 @@ package Day23;
 
 //Write a program to Find first repeating character.
 
+import java.util.Scanner;
+
 public class Q90 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Write text: ");
+        String text = sc.nextLine();
+
+        String lowerText = text.toLowerCase();
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < lowerText.length(); i++) {
+            freq[lowerText.charAt(i)]++;
+        }
+
+        char repeatingChar = '\0';
+
+        for (int i = 0; i < lowerText.length(); i++) {
+            char c = lowerText.charAt(i);
+            if (freq[c] != 1) {
+                repeatingChar = c;
+                break;
+            }
+        }
+
+        if (repeatingChar != '_') {
+            System.out.println("First repeating character in " + "\"" + text + "\" is: " + repeatingChar);
+        } else {
+            System.out.println("No repeating character found in " + "\"" + text + "\"");
+        }
+
+        sc.close();
+    }
 }
