55eb02ce2d886f562860441e4ba1d6b9b7667052
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-22 22:50:49 +0530

    Complete Q88

--- Day22/Q88.java
+++ Day22/Q88.java
@@ -2,6 +2,19 @@ package Day22;
 
 //Write a program to Remove spaces from string.
 
+import java.util.Scanner;
 public class Q88 {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Write the sentence: ");
+        String text = sc.nextLine();
+
+        String replacedText = text.replace(" ", "");
+
+        System.out.print("Sentence after removing spaces: " + replacedText);
+
+        sc.close();
+    }
 }
