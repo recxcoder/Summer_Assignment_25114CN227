2c5c2c5246a2e7905bc33a012e9d71bf46ad3b15
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-24 17:46:12 +0530

    Complete Q95

--- Day24/Q95.java
+++ Day24/Q95.java
@@ -2,6 +2,28 @@ package Day24;
 
 //Write a program to Find longest word.
 
+import java.util.Scanner;
 public class Q95 {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Write sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+
+        String longesteWord = " ";
+
+        for (String word : words) {
+            if (word.length() > longesteWord.length()) {
+                longesteWord = word;
+            }
+        }
+
+        System.out.println("Longest word from given sentence is: " + longesteWord);
+        System.out.println("Length of the word is: " + longesteWord.length());
+
+        sc.close();
+    }
 }
