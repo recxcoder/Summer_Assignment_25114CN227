6b73606100109b980cb38fcac63aafd462941523
Shivam Vishwakarma <recxcoder@gmail.com>
2026-06-24 17:59:24 +0530

    Complete Q96

--- Day24/Q96.java
+++ Day24/Q96.java
@@ -2,6 +2,30 @@ package Day24;
 
 //Write a program to Remove duplicate characters.
 
+import java.util.Scanner;
+import java.util.HashSet;
+
 public class Q96 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        HashSet<Character> checked = new HashSet<>();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (checked.add(ch)) {
+                result.append(ch);
+            }
+        }
+
+        System.out.println("String after removing duplicates: " + result);
+
+        sc.close();
+    }
 }
